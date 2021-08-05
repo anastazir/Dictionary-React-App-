492cca2847a77808d954efd0dad165cbbf6f183b
Anas Tazir <atazir2018@gmail.com>
2021-08-05 18:57:45 +0530

    import axios and verify api URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+/* eslint-disable no-unused-vars */
 import './App.css';
-
+import {useEffect} from 'react';
+import axios from 'axios';
 function App() {
+  
+  const dictionaryAPI= async() =>{
+    try {
+      const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane")
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    dictionaryAPI()
+  }, [])
   return (
+
     <div className="App">
       asdfasdfasd
     </div>
