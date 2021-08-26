31316da9989c6ee41bad162038cc68c181761b49
Anas Tazir <atazir2018@gmail.com>
2021-08-26 23:19:01 +0530

    make and style a functioning meaning block

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import Header from './components/Header/Header';
 import Definitions from './components/Definitions/Definitions.js';
 function App() {
   const [word, setWord] = useState('')
-  const [meaning, setMeaning] = useState([])
+  const [meanings, setMeanings] = useState([])
   const [category, setCategory] = useState("en")
 
   const dictionaryAPI= async() =>{
     try {
       const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`)
-      setMeaning(data.data)
+      setMeanings(data.data)
     } catch (error) {
       console.log(error);
     }
   }
 
-  console.log(meaning);
+  // console.log(meanings);
   useEffect(() => {
     dictionaryAPI()
   }, [word, category])
@@ -29,7 +29,7 @@ function App() {
     <div className="App" style={{height: '100vh', background: '#282c34', color: 'white'}}>
       <Container maxWidth="md" style= {{display: "flex", flexDirection: "column", height: "100vh"}}>
           <Header category={category} setCategory={setCategory} word={word} setWord={setWord}/>
-          <Definitions/>
+          {meanings && <Definitions word={word} meanings={meanings} category={category}/>}
       </Container>
     </div>
   );
--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import "./Definitions.css"
 
-const Definitions = () => {
+const Definitions = ({word, category, meanings}) => {
     return (
-        <div>
-            afasdfas
+        <div className="meanings">
+            {   
+                word===""? (<span className="subTitle">Start by typing a word in the search box</span>):
+                meanings.map((mean)=>
+                    mean.meanings.map((item)=>
+                        item.definitions.map((definition)=>
+
+                            <div className="singleMeaning" style={{background: 'white', color: 'black'}} >
+                                <b>{definition.definition}</b>    
+                                <hr style={{background: 'white', width: '100%'}}></hr>
+                            </div>
+
+                        )
+                    )
+                )
+            }
         </div>
+        
     )
 }
 
