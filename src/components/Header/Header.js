f1ccddca84a11a13cf331cfb97e65c1992b04a19
Anas Tazir <atazir2018@gmail.com>
2021-08-26 15:28:47 +0530

    style the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css"
 
 export default function Header() {
     return (
-        <div>
+        <div className="header">
             <span className="title">Word Hunt</span>
         </div>
     )
