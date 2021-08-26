37549bacdcc1401da47e97fb9d13f2b8d434f2b4
Anas Tazir <atazir2018@gmail.com>
2021-08-26 15:45:52 +0530

    add a file with language names and their short forms

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
-import {createTheme, TextField, ThemeProvider} from "@material-ui/core"
+import {createTheme, MenuItem, TextField, ThemeProvider} from "@material-ui/core"
 import React from 'react'
 import "./Header.css"
-
+import category from "../../data/category"
 export default function Header() {
     const darkTheme= createTheme({
         palette: {
@@ -17,6 +17,15 @@ export default function Header() {
             <div className="inputs">
             <ThemeProvider theme={darkTheme}>
                 <TextField id="standard-basic" label="Standard" />
+                <TextField
+                    id="standard-select-currency"
+                    select
+                    label="Select"
+                    helperText="Please select your currency">
+                    <MenuItem >
+                        English
+                    </MenuItem>
+                </TextField>
             </ThemeProvider>
             </div>
         </div>
