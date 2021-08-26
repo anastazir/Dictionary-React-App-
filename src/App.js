5285759bca51d74e25fea4730c49bf633bf6d86e
Anas Tazir <atazir2018@gmail.com>
2021-08-26 15:19:39 +0530

    add a new file Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 /* eslint-disable no-unused-vars */
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
+import { Container } from '@material-ui/core';
 function App() {
-  
+  const [word, setWord] = useState('')
+  const [meaning, setMeaning] = useState([])
+
   const dictionaryAPI= async() =>{
     try {
       const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/plane")
-      console.log(data);
+      setMeaning(data.data)
     } catch (error) {
       console.log(error);
     }
   }
 
+  console.log(meaning);
   useEffect(() => {
     dictionaryAPI()
   }, [])
-  return (
 
-    <div className="App">
-      asdfasdfasd
+  return (
+    <div className="App" style={{height: '100vh', background: '#282c34', color: 'white'}}>
+      <Container maxWidth="md" style= {{display: "flex", flexDirection: "column", height: "100vh"}}>
+          Dictionary
+      </Container>
     </div>
   );
 }
