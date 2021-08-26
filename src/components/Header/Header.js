877180474084174cb6be6debf54772a53345640e
Anas Tazir <atazir2018@gmail.com>
2021-08-26 15:36:48 +0530

    add and styel text field

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,24 @@
+import {createTheme, TextField, ThemeProvider} from "@material-ui/core"
 import React from 'react'
 import "./Header.css"
 
 export default function Header() {
+    const darkTheme= createTheme({
+        palette: {
+            primary:{
+                main: '#fff'
+            },
+            type: 'dark',
+        },
+    })
     return (
         <div className="header">
             <span className="title">Word Hunt</span>
+            <div className="inputs">
+            <ThemeProvider theme={darkTheme}>
+                <TextField id="standard-basic" label="Standard" />
+            </ThemeProvider>
+            </div>
         </div>
     )
 }
