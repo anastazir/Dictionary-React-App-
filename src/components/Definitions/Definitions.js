14787c8429872d2e0528a7781645cf5e561183ae
Anas Tazir <atazir2018@gmail.com>
2021-08-27 21:48:21 +0530

    add line breaks

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -11,7 +11,22 @@ const Definitions = ({word, category, meanings}) => {
                         item.definitions.map((definition)=>
 
                             <div className="singleMeaning" style={{background: 'white', color: 'black'}} >
-                                <b>{definition.definition}</b>    
+                                <b>{definition.definition} &nbsp;</b>    
+                                <br></br>
+                                {definition.example && (
+                                    <span>
+                                        <b>Example : </b>
+                                        {definition.example} &nbsp;
+                                    </span>
+                                )}
+                                <br></br>
+                                {definition.synonyms && (
+                                    <span>
+                                        <b>Synonyms : </b>
+                                        {definition.synonyms.map((synonym)=> `${synonym}, `)}
+                                    </span>
+                                )}
+
                                 <hr style={{background: 'white', width: '100%'}}></hr>
                             </div>
 
